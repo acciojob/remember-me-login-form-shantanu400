19a5c81a992017bbf68c96ee13d97e7187bf88fd
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-23 17:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let save=document.getElementById('submit').addEventListener('click',function(){
+	let user==document.getElementById('Username').value;
+	let pass==document.getElementById('password').value;
+	if((localStorage.getItem("Username")==user) && (localStorage.getItem("password")==pass)){
+		document.getElementById('submit').innerHTML+=`
+		<button type="submit" id="existing" value=" submit">Login as existing user</button>
+		`;
+		document.getElementById('existing').addEventListener('click',fuction(){
+			alert('Logged in as');
+		})
+	}
+	else{
+		if(document.getElementById('checkbox').checked){
+			localStorage.setItem("Username","user");
+			localStorage.setItem("password","pass");
+		
+	}
+	else{
+		localStorage.removeItem("Username");
+		localStorage.removeItem("password");
+		
+	}
+	alert('Logged in as');
+
+})
+	}
+	
