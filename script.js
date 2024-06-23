07886889759d414aef01ca1dbf7ffd2b32dd9022
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-23 18:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-//your JS code here. If required.
 let save=document.getElementById('submit').addEventListener('click',function(){
-	let user==document.getElementById('Username').value;
-	let pass==document.getElementById('password').value;
+	let user=document.getElementById('Username').value;
+	let pass=document.getElementById('password').value;
 	if((localStorage.getItem("Username")==user) && (localStorage.getItem("password")==pass)){
-		document.getElementById('submit').innerHTML+=`
+		let a=document.getElementById('addnew');
+			a.innerHTML+=`
 		<button type="submit" id="existing" value=" submit">Login as existing user</button>
 		`;
-		document.getElementById('existing').addEventListener('click',fuction(){
-			alert('Logged in as');
-		})
+		document.getElementById('existing').addEventListener('click',function(){
+			alert('Logged in as'+ user);
+		}
+		)
 	}
 	else{
 		if(document.getElementById('checkbox').checked){
 			localStorage.setItem("Username","user");
 			localStorage.setItem("password","pass");
-		
-	}
-	else{
-		localStorage.removeItem("Username");
-		localStorage.removeItem("password");
-		
-	}
-	alert('Logged in as');
-
-})
-	}
+		alert('Logged in as'+ user);
+			}
+			else{
+				alert('Logged in as'+ user);
+				localStorage.removeItem("Username");
+				localStorage.removeItem("password");	
+			}	
+		}
+}
 	
